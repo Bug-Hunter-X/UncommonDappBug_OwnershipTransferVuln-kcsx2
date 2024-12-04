c2508403154feac2c159e8654df26838515206df
BugHunter-X <askzenai@gmail.com>
2024-12-04 15:46:09 +0100

    Add bug.sol file

---
+++ bug.sol
@@ -0,0 +1,14 @@
+function transferOwnership(newOwner) {
+  require(newOwner != address(0), "Ownable: new owner is the zero address");
+  emit OwnershipTransferred(_owner, newOwner);
+  _owner = newOwner;
+}
+
+// ... other code ...
+
+// this is the function that is intended to be called from external contract
+function updateData(newData) {
+  // Check if the caller is the contract owner before updating the data.
+  require(msg.sender == owner(), "Only the owner can update the data");
+  data = newData;
+}
